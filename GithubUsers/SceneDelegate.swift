d74349515fbf8f3b2802ae67dec9970b7475bd64
José Lucas Souza das Chagas <chagasjoselucas@gmail.com>
2023-04-13 16:21:23 -0300

    Add NavigationController and init ListUsersVC using its corresponding factories

--- GithubUsers/SceneDelegate.swift
+++ GithubUsers/SceneDelegate.swift
@@ -19,7 +19,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: windowScene.coordinateSpace.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = ViewController()
+        
+        let navController = UINavigationController(rootViewController: ListUsersVCFactory.make())
+        window?.rootViewController = navController
         window?.makeKeyAndVisible()
     }
 
